4732c3293a663c445a0cf062a979f47b181f44d4
Horacio Peña Mendoza <hpenam@uaemex.mx>
2022-10-12 10:58:54 -0500

    Se realiza instalación de quasar, funcionando ok!

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+
+// Import icon libraries
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+import '@quasar/extras/themify/themify.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,5 +20,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(Quasar, {
+  plugins: {},
+});
 app.mount("#app");
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="login">Mi login</div>
+  <q-btn color="primary" icon="check" label="OK" @click.prevent="submit" />
 </template>
+<script setup>
+
+const submit = ()=> {
+  alert("working!");
+}
+</script>
 
 <style scoped lang="scss" src="../assets/scss/login_main.scss" />
