6c2f69c0595dfcb493ef745200622029fd29eab5
Horacio Peña Mendoza <hpenam@uaemex.mx>
2022-10-14 17:19:45 -0500

    Creacion de login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="login">Mi login</div>
-  <q-btn color="primary" icon="check" label="OK" @click.prevent="submit" />
+  <div class="login__container">
+    <ul class="login__navigation">
+      <li class="login__navigation--item active"><a href="#">Entrar</a></li>
+      <li class="login__navigation--item"><a href="#">Registrarse</a></li>
+    </ul>
+    <form action="submit" class="login__form">
+      <q-input
+        v-model="email"
+        color="indigo-3"
+        input-style="color:white"
+        label-color="indigo-3"
+        label="Email"
+        type="text"
+      />
+      <q-input
+        v-model="password"
+        color="indigo-3"
+        input-style="color:white"
+        label-color="indigo-3"
+        label="Password"
+        type="password"
+      />
+      <!-- <label for="login-sign-up" class="login__label--checkbox">
+			<input id="login-sign-up" type="checkbox" class="login__input--checkbox" />
+			Keep me Signed in
+		</label> -->
+      <button class="login__submit" @submit="submit">Entrar</button>
+    </form>
+    <a href="#" class="login__forgot">¿Olvidaste tu Contraseña?</a>
+  </div>
 </template>
 <script setup>
+import { ref } from "vue";
 
-const submit = ()=> {
+const email = ref("");
+const password = ref("");
+
+const submit = () => {
   alert("working!");
-}
+};
 </script>
 
 <style scoped lang="scss" src="../assets/scss/login_main.scss" />
